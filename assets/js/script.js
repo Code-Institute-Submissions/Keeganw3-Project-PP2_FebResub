c96f33e4e91cabec9808f1f7b7cd6f1e6b58aaa6
Keeganw3 <keeganw749@gmail.com>
2021-12-17 23:13:06 +0000

    Added questions structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
 const questionContainer = document.getElementById('question-container')
 console.log(questionContainer)
+
+function questions() {
+    {
+        'question': 'What is the best language?',
+        'a': 'JS',
+        'b': 'HTML',
+        'c': 'CSS',
+        'd': 'Python',
+        'correctAnswer': 'd'
+      },
+      {
+        'question': 'Who is the president?',
+        'a': 'John',
+        'b': 'James',
+        'c': 'Joe',
+        'd': 'Micheal',
+        'correctAnswer': 'a'
+      }  
+    ]
+}
